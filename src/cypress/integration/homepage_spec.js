7f406782c8c780e58ad4a360040dfc72c9c66841
Sarah Lane <sarahlane8@gmail.com>
2021-05-26 11:34:14 -0600

    Add form rendering to page cypress test

--- src/cypress/integration/homepage_spec.js
+++ src/cypress/integration/homepage_spec.js
@@ -8,6 +8,11 @@ describe ('Homepage', () => {
   })
 
   it('should render a form on pageload', () => {
-    cy.get()
+    cy.get('form').should('be.visible')
+      .get('form > input:first').invoke('attr', 'placeholder').should('contain', 'Name')
+      .get('form >input').eq(1).invoke('attr', 'placeholder').should('contain', 'Date (mm/dd)')
+      .get('form >input').eq(2).invoke('attr', 'placeholder').should('contain', 'Time')
+      .get('form >input:last').invoke('attr', 'placeholder').should('contain', 'Number of guests')
+      .get('button').contains('Make Reservation')
   })
 })
