a8740b83ea1cb2d6eaa7be9757d013b974eda6db
Sarah Lane <sarahlane8@gmail.com>
2021-05-26 10:33:28 -0600

    Add input fields and handlechange method

---
+++ src/components/Form/Form.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react'
+
+class Form extends Component {
+  constructor() {
+    super()
+  }
+  this.state = {
+    name: '',
+    date: '',
+    time: '',
+    number: ''
+  }
+
+handleChange = event => {
+  this.setState({
+    [event.target.name]: event.target.value
+  })
+}
+
+  render() {
+    return (
+      <form className='form' >
+          <input
+            type="text"
+            name='name'
+            value={this.state.name}
+            placeholder="Name"
+            onChange={event => this.handleChange(event)}
+          />
+
+          <input
+            type="text"
+            name='date'
+            value={this.state.date}
+            placeholder="Date (mm/dd)"
+            onChange={event => this.handleChange(event)}
+          />
+
+          <input
+            type="text"
+            name='time'
+            value={this.state.time}
+            placeholder="Time"
+            onChange={event => this.handleChange(event)}
+          />
+
+          <input
+            type="text"
+            name='number'
+            value={this.state.number}
+            placeholder="Number of guests"
+            onChange={event => this.handleChange(event)}
+          />
+    )
+  }
+
+
+}
+
+
+
+
+export default Form
