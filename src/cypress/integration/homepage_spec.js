d7fc5cb288cfbaa89433179edaeb0ab2d53c07f2
Sarah Lane <sarahlane8@gmail.com>
2021-05-26 11:41:55 -0600

    Add cypress tests

--- src/cypress/integration/homepage_spec.js
+++ src/cypress/integration/homepage_spec.js
@@ -15,4 +15,31 @@ describe ('Homepage', () => {
       .get('form >input:last').invoke('attr', 'placeholder').should('contain', 'Number of guests')
       .get('button').contains('Make Reservation')
   })
+
+  it('should display existing reservation cards on page load', () => {
+    cy.get('.card').should('have.length', 9)
+      .get('.card:first').contains('Christie')
+  })
+
+  it('should show the input value as the user is typing', () => {
+    cy.get('form >input:first').type('Sarah')
+      .should('have.value', 'Sarah')
+      .get('form >input').eq(1).type('06/24')
+      .should('have.value', '06/24')
+      .get('form >input').eq(2).type('7')
+      .should('have.value', '7')
+      .get('form >input:last').type('3')
+      .should('have.value', '3')
+  })
+
+  it('should add a card when a reservation is made', () => {
+    cy.get('.card').should('have.length', 9)
+      .get('form >input:first').type('Sarah')
+      .get('form >input').eq(1).type('06/24')
+      .get('form >input').eq(2).type('7')
+      .get('form >input:last').type('3')
+      .get('button').click()
+      .get('.card').should('have.length', 10)
+
+  })
 })
