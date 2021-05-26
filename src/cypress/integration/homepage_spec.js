490547078df2512787437ca367b61ea91de6fa67
Sarah Lane <sarahlane8@gmail.com>
2021-05-26 11:22:24 -0600

    Fix syntax to run first test

--- src/cypress/integration/homepage_spec.js
+++ src/cypress/integration/homepage_spec.js
@@ -1,10 +1,13 @@
 describe ('Homepage', () => {
   beforeEach( () => {
     cy.visit('http://localhost:3000/')
-  )}
+  })
 
   it('should render a header on pageload', () => {
     cy.get('h1').contains('Turing Cafe Reservations')
   })
-  
+
+  it('should render a form on pageload', () => {
+    cy.get()
+  })
 })
