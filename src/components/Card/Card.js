3bed4d4ab6ca68b6dc97a377d2a2b0f7604fe5f9
Sarah Lane <sarahlane8@gmail.com>
2021-05-26 10:11:57 -0600

    Build out Card component

---
+++ src/components/Card/Card.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Card = ({ id, name, date, time, number }) => {
+  return (
+    <div className='card'
+      <h2>{name}</h2>
+      <p>{date}</p>
+      <p>{time}</p>
+      <p>Number of guests: {number}</p>
+    </div>
+  )
+}
+
+
+export default Card
