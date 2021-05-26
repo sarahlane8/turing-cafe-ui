1ddd68b551ef2e82f49e221a020598cb21903e38
Sarah Lane <sarahlane8@gmail.com>
2021-05-26 09:28:24 -0600

    Add getAllReservation fetch to apiCalls

---
+++ src/apiCalls.js
@@ -0,0 +1,7 @@
+const getAllReservations = () => {
+  return fetch('http://localhost:3001/api/v1/reservations')
+    .then(response => response.json())
+    .catch(err => console.log(err))
+}
+
+export { getAllReservations }
